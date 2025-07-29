0e9dd10a0516a0f4f5152d8f15b44b3ec4060d25
Chisanan232 <chi10211201@cycu.org.tw>
2025-07-29 09:17:29 +0800

    ➕ Add a new MCP server function gets team info.

--- clickup_mcp/mcp_server/__init__.py
+++ clickup_mcp/mcp_server/__init__.py
@@ -5,5 +5,6 @@ This module exports all MCP functions provided by the clickup-mcp-server.
 """
 
 from .space import get_space
+from .team import get_authorized_teams
 
-__all__ = ["get_space"]
+__all__ = ["get_space", "get_authorized_teams"]
