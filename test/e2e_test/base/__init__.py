9223195818feacc063104aa49e870918b1ec4abd
Chisanan232 <chi10211201@cycu.org.tw>
2025-08-10 16:15:29 +0800

    ♻️ Add some description for the new test subpackage.

---
+++ test/e2e_test/base/__init__.py
@@ -0,0 +1,17 @@
+"""
+Common logic for end-to-end tests that need to set up and run a server to verify
+features truly.
+
+This subpackage contains modules that are used by the end-to-end tests that need
+to set up and run a server to verify features truly. These tests are typically
+run in a CI environment and are used to catch regressions before they reach the
+production environment.
+
+The modules in this subpackage are responsible for:
+
+* Setting up the server and preparing the environment for the tests
+* Running the tests using the specific transport type (e.g. HTTP streaming,
+  SSE, etc.)
+* Cleaning up the environment after the tests have completed
+
+"""
