9968d8ecc1799235d88a2292f715b562166103f7
Chisanan232 <chi10211201@cycu.org.tw>
2025-08-13 21:14:07 +0800

    ➕ Add the sidebar setting for the navigation *Document*.

---
+++ docs/sidebars-documentation.ts
@@ -0,0 +1,59 @@
+import type {SidebarsConfig} from '@docusaurus/plugin-content-docs';
+
+/**
+ * Sidebar for the Documentation section
+ */
+const sidebars: SidebarsConfig = {
+  documentation: [
+    'introduction',
+    {
+      type: 'category',
+      label: '🤟 Quickly Start',
+      collapsed: false,
+      items: [
+        'quick-start/quick-start',
+        'quick-start/requirements',
+        'quick-start/installation',
+        'quick-start/how-to-run',
+      ],
+    },
+    {
+      type: 'category',
+      label: '🧑‍💻 API References',
+      items: [
+        'api-references/api-references',
+        {
+          type: 'category',
+          label: 'Web APIs',
+          items: [
+            'api-references/web-apis/web-apis',
+            'api-references/web-apis/team-web-api',
+            'api-references/web-apis/space-web-api',
+          ],
+        },
+        {
+          type: 'category',
+          label: 'MCP Server APIs',
+          items: [
+            'api-references/mcp-apis/mcp-apis',
+            'api-references/mcp-apis/team-mcp-api',
+            'api-references/mcp-apis/space-mcp-api',
+          ],
+        },
+      ],
+    },
+    {
+      type: 'category',
+      label: '👋 Welcome to contribute',
+      items: [
+        'contribute/contribute',
+        'contribute/report-bug',
+        'contribute/request-changes',
+        'contribute/discuss',
+      ],
+    },
+    'changelog',
+  ],
+};
+
+export default sidebars;
