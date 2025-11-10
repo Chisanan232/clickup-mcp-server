712a106363dc04e4e17182b63647d4de8c7b1a91
Chisanan232 <chi10211201@cycu.org.tw>
2025-11-10 12:04:46 +0800

    ➕ Export the DTO models of the ClickUp features.

---
+++ clickup_mcp/models/dto/__init__.py
@@ -0,0 +1,26 @@
+"""
+DTOs for ClickUp API requests and responses.
+
+This module exports all DTO classes for interacting with the ClickUp API.
+"""
+
+from .folder import FolderCreate, FolderResp, FolderUpdate
+from .list import ListCreate, ListResp, ListUpdate
+from .space import SpaceCreate, SpaceResp, SpaceUpdate
+from .task import TaskCreate, TaskListQuery, TaskResp, TaskUpdate
+
+__all__ = [
+    "SpaceCreate",
+    "SpaceResp",
+    "SpaceUpdate",
+    "FolderCreate",
+    "FolderResp",
+    "FolderUpdate",
+    "ListCreate",
+    "ListResp",
+    "ListUpdate",
+    "TaskCreate",
+    "TaskResp",
+    "TaskUpdate",
+    "TaskListQuery",
+]
